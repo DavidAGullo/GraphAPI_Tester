1b2da87cd19fa929360e17c1309b3bec8e61375b
david <david@DAVIDSGAMINGPC>
2024-06-27 11:38:55 -0500

    Add project files.

---
+++ HomeContentForm.cs
@@ -0,0 +1,10 @@
+namespace GraphAPI_Tester
+{
+    public partial class HomeContentForm : Form
+    {
+        public HomeContentForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
